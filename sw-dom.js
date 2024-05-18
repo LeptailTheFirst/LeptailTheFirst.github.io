7c5719ffdb215cc7f862c840d89d61f05b62d112
LynxCatTheThird <tsfxlyx@qq.com>
2024-05-18 11:41:05 +0800

    Site updated: 2024-05-18 11:41:03

--- sw-dom.js
+++ sw-dom.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
             caches.match(location.href).then(res => {
                 if (res)
                     res.json().then(json => {
-                        utils && utils.snackbarShow(`已刷新缓存，更新为${json.global + '.' + json.local}版本最新内容`, false, 2000);
+                        console.log(`已刷新缓存，更新为${json.global + '.' + json.local}版本最新内容`);
                     });
                 else
                     console.info('未找到缓存');
