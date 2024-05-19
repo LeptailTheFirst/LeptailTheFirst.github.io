968025759a646994adac9ac54ec3564951d9fb52
LynxCatTheThird <tsfxlyx@qq.com>
2024-05-19 11:45:43 +0800

    Site updated: 2024-05-19 11:45:43

--- sw.js
+++ sw.js
@@ -109,7 +109,7 @@ cdn: {
 clean: true,
 match: url => [
             "cdn.staticfile.org",
-            // "cdn.staticfile.net",
+            "cdn.staticfile.net",
             // "cdn.bootcdn.net",
             "jsd.cdn.zzko.cn",
             // "jsd.onmicrosoft.cn",
@@ -117,7 +117,7 @@ match: url => [
             // "www.clarity.ms",
             // "s4.zstatic.net",
             "s2.hdslb.com",
-            // "fonts.googleapis.com",
+            "fonts.googleapis.com",
         ].includes(url.host) && url.pathname.match(/\.(js|css|woff2|woff|ttf|json|png|jpg|webp)$/)}
 }
 
